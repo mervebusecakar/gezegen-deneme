e30f371d625f5a8065bb5429f7bc2b8b864c55dc
mervebusecakar <56163566+mervebusecakar@users.noreply.github.com>
2022-10-17 17:12:27 +0300

    Add files via upload

---
+++ planet/PlanetMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMovement : MonoBehaviour
+{
+    public float orbitSpeed;
+    public float orbitPeriod=0f;
+
+    [Range(0f, 1f)]
+    public float orbitProgress;
+    public ellipse orbitPath;// = new ellipse();
+    public bool orbitActive;
+
+    public Transform movingPlanet;
+
+    private void SetOrbitObject()
+    {
+        Vector2 orbit = orbitPath.Evaluate(orbitProgress);
+        movingPlanet.position = new Vector3(orbit.x, 0f, orbit.y);
+
+    }
+    private void Start()
+    {
+        StartCoroutine(StartOrbiting());
+    }
+
+    private IEnumerator StartOrbiting()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        if (orbitPeriod < 0.1f)
+            orbitPeriod = .1f;
+
+        float speed = 1 / orbitPeriod;
+
+        while (orbitActive)
+        {
+            orbitProgress += Time.deltaTime * speed;
+            orbitProgress %= 1;
+            SetOrbitObject();
+            yield return null;
+        }
+    }
+}
---
+++ planet/planetScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetScript : MonoBehaviour
+{
+    public float planetSizeRU;
+    public double semiMajorAxisRU;
+    public double semiMinorAxisRU;
+
+    public float orbitPeriod;
+}
